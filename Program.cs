ec42bd3506f303d11450bfdca0ae18440d75fc57
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-16 10:10:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_two_numbers_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int temp;
+
+            a = -13;
+            b = 2;
+
+
+            temp = a;
+            a = b;
+            b=temp;
+
+          Console.WriteLine("\n After swapping:" );
+          Console.WriteLine("\n a= "+ a);
+            Console.WriteLine("\n b= "+ b);
+
+            Console.Read();
+        }
+    }
+}
